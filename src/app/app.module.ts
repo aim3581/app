1360ae614343b577a9f31126f5083f2cfd92335e
Nupoor D <nupoor8097@gmail.com>
2019-05-30 21:11:49 +0530

    product-list component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { BookServiceService } from './book-service.service';
 import { BookListComponent } from './book-list/book-list.component';
 import { AddBookComponent } from './add-book/add-book.component';
 import { TestAddComponent } from './test-add/test-add.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
     AddBookComponent,
-    TestAddComponent
+    TestAddComponent,
+    ProductListComponent
     ],
   imports: [
     BrowserModule,
